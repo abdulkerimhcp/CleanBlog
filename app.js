34ad0115dd67d2f1293972a9c9b6528bd1a5ddca
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-10-12 14:47:43 +0300

    Clean Blog Projesi 2. Bölüm

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
-});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-const port = 3000;
+app.get('/', (req, res) => {
+ res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+ });
+ app.get('/post', (req, res) => {
+  res.render('post')
+ });
+ app.get('/add_post', (req, res) => {
+  res.render('add_post')
+ });
+
+const port = 5858;
 app.listen(port, () => {
   console.log(`Sunucu http://localhost:${port} başlatıldı..`);
 });
