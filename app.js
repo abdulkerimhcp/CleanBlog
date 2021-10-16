da8ad4f48428b7066bedcec7e6d7c237cc65b835
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-10-16 14:35:16 +0300

    Clean Blog Projesi 4. Bölüm

--- app.js
+++ app.js
@@ -11,16 +11,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
   const posts = await Post.find({});
   res.render('index', { posts });
 });
+
+app.get('/post/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post', { post });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
-app.get('/post', (req, res) => {
-  res.render('post');
-});
+
 app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
